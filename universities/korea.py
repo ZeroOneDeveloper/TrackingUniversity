c89883d10769e4c0f9caa1f956b6c92738750f43
ZeroOneDeveloper <me@zero0ne.dev>
2024-09-09 23:06:45 +0900

    refactor: change color of korea university

--- universities/korea.py
+++ universities/korea.py
@@ -1,4 +1,3 @@
-import asyncio
 from bs4 import BeautifulSoup
 from aiohttp import ClientSession
 
@@ -6,7 +5,7 @@ from dataStructure import CompetitionRate
 
 
 async def main() -> CompetitionRate:
-    url = "http://addon.jinhakapply.com/RatioV1/RatioH/Ratio10080231.html"
+    url = "https://ratio.uwayapply.com/Sl5KOGB9YTlKZiUmOiZKI2ZUZg=="
 
     async with ClientSession() as session:
         async with session.get(url) as response:
@@ -15,22 +14,20 @@ async def main() -> CompetitionRate:
     current = soup.select_one("#table > table:nth-child(5) > tbody:nth-child(3)")
     updatedAt = soup.select_one("#ID_DateStr > label").text.strip()
 
-    KAIST = CompetitionRate(
-        universityName="건국대학교(서울)",
+    KOREA = CompetitionRate(
+        universityName="고려대학교",
         siteUri=url,
         updatedAt=updatedAt,
-        universityColor=(0, 65, 145),
+        universityColor=(134, 38, 51),
     )
 
     for tr in current.select("tr"):
         tds = tr.select("td")
-        KAIST.addParagon(
+        KOREA.addParagon(
             paragon=tds[0].text,
             totalMember=tds[1].text,
             member=tds[2].text,
             competitionRate=tds[3].text,
         )
 
-    return KAIST
-
-print(asyncio.run(main()).toList())
+    return KOREA
