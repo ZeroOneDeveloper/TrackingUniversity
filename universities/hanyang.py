ba6f1ee3ea0dc819236eef0af4ef7b341eb0e716
ZeroOneDeveloper <me@zero0ne.dev>
2024-09-10 15:34:52 +0900

    fix: updating hanyang university

--- universities/hanyang.py
+++ universities/hanyang.py
@@ -5,29 +5,30 @@ from dataStructure import CompetitionRate
 
 
 async def main() -> CompetitionRate:
-    url = "http://ratio.uwayapply.com/Sl5KTjlKZiUmOiZKI2ZUZg=="
+    url = "http://addon.jinhakapply.com/RatioV1/RatioH/Ratio11640371.html"
 
     async with ClientSession() as session:
         async with session.get(url) as response:
             response = await response.text()
     soup = BeautifulSoup(response, "html.parser")
-    current = soup.select_one("#table > table > tbody:nth-child(3)")
-    updatedAt = soup.select_one("#ID_DateStr > label").text.strip()
+    
+    current = soup.select_one("#Ratio1164037 > div:nth-child(1) > table")
+    updatedAt = soup.select_one("#RatioTime").text.strip()
 
-    Kwangwoon = CompetitionRate(
-        universityName="광운대학교",
+    Hanyang = CompetitionRate(
+        universityName="한양대학교",
         siteUri=url,
         updatedAt=updatedAt,
-        universityColor=(119, 34, 46),
+        universityColor=(14,74,132),
     )
 
     for tr in current.select("tr")[1:]:
         tds = tr.select("td")
-        Kwangwoon.addParagon(
-            paragon=tds[0].text,
-            totalMember=tds[1].text,
-            member=tds[2].text,
-            competitionRate=tds[3].text,
-        )
+        Hanyang.addParagon(
+                paragon=tds[0].text,
+                totalMember=tds[1].text,
+                member=tds[2].text,
+                competitionRate=tds[3].text,
+            )
 
-    return Kwangwoon
+    return Hanyang
